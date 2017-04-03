fe362b9885414235c0a5ac4f6dff9ff6d0fab1fd
adamliu84 <adam.liu84@gmail.com>
2017-04-02 19:59:28 -0700

    minimum-sum-absolute-difference-pairs-two-arrays

---
+++ MinSumAbsDiff.hs
@@ -0,0 +1,11 @@
+import Data.List
+
+findMinSumAbsDiff :: [Int] -> [Int] -> Int
+findMinSumAbsDiff a b = sum $ zipWith (\x y -> abs $ x-y) sa sb
+    where sa = sort a
+          sb = sort b
+
+main :: IO ()
+main = do
+    print $ findMinSumAbsDiff [3,2,1] [2,1,3]
+    print $ findMinSumAbsDiff [4,1,8,7] [2,3,6,5]
