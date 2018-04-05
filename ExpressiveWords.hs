f41924301d4b242722e96a41f067c97f7b2dc832
adamliu84 <adam.liu84@gmail.com>
2018-04-05 20:25:06 +0800

    Committing expressiveWords

---
+++ ExpressiveWords.hs
@@ -0,0 +1,26 @@
+import Data.List
+
+type CharAndCount = (Char, Int)
+
+groupAndCount :: String -> [CharAndCount]
+groupAndCount s = map (\x-> (x!!0, length x)) $ group s
+
+expressiveWords :: String -> [String] -> Int
+expressiveWords s words =
+    expressiveWords' (groupAndCount s) words
+    where expressiveWords' :: [CharAndCount] -> [String] -> Int
+          expressiveWords' _ [] = 0
+          expressiveWords' s (w:ws) = checkWord s (groupAndCount w) + expressiveWords' s ws
+
+checkWord :: [CharAndCount] -> [CharAndCount] -> Int
+checkWord [] [] = 1
+checkWord [] _ = 0
+checkWord _ [] = 0
+checkWord ((bc,bl):bs) ((wc,wl):ws)
+    | bc /= wc = 0
+    | bl <= 2 && bl /= wl = 0
+    | otherwise = checkWord bs ws
+
+main :: IO ()
+main = do
+    print $ expressiveWords "heeellooo" ["hello", "hi", "helo"]
